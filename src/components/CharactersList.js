20ef23348831d5bfe6ca05780e23c38002ddadeb
joana <joanabrito216@gmail.com>
2023-09-02 17:53:42 +0100

    Changes in styling

--- src/components/CharactersList.js
+++ src/components/CharactersList.js
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom"
 import axios from "axios"
 import MissingImage from "./MissingImage"
 
+
+
 export default function CharactersList() {
   const [characters, setCharacters] = useState([])
   // console.log(characters)
@@ -13,7 +15,7 @@ export default function CharactersList() {
     async function getCharacterData() {
       try {
         const { data } = await axios('/character', { params: { pageSize: 500 } })
-      
+
         // console.log(data)
         setCharacters(data.data)
       } catch (error) {
@@ -22,31 +24,28 @@ export default function CharactersList() {
     }
     getCharacterData()
   }, [])
-//  Current change - What you have in your local repo
-// In-coming change - Is what is currently in the remote repository
+  //  Current change - What you have in your local repo
+  // In-coming change - Is what is currently in the remote repository
 
   return (
     <>
-      <h1>Hello</h1>
+      <img src='/images/disney.gif' alt="Disney-gif"/>
       <section className="container">
-      {characters.length > 0 ? characters.map((character => {
-        const characterName = character.name
-        return (
-          
-          <div className="character-list character" key={character._id}>
-            <h2>{characterName}</h2>
-            <Link to={`/characters/${character._id}`}>
-              {/* <img alt={'disneycharacter'} src={character.imageUrl} /> */}
-              <MissingImage key={`missingImage-${character._id}`} imageUrl={character.imageUrl} altText={character.name} 
-              placeHolderImageUrl={'https://static.wikia.nocookie.net/disney/images/7/7c/Noimage.png'}  />
+        {characters.length > 0 ? characters.map((character => {
+          const characterName = character.name
+          return (
+
+            <div className="character-list character" key={character._id}>
+              <h2>{characterName}</h2>
+              <Link to={`/characters/${character._id}`}>
+                {/* <img alt={'disneycharacter'} src={character.imageUrl} /> */}
+                <MissingImage key={`missingImage-${character._id}`} imageUrl={character.imageUrl} altText={character.name}
+                  placeHolderImageUrl={'https://static.wikia.nocookie.net/disney/images/7/7c/Noimage.png'} />
               </Link>
-          </div>
-         
-        )
-      })) : 'Loading'}
-     </section>
-      {/* </Container> */}
+            </div>
+          )
+        })) : 'Loading'}
+      </section>
     </>
   )
 }
-// <section className="container">
