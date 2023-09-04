b781b993ef221c5e505f62b6d6d3ae2088a47038
kisalier <kisalie@hotmail.com>
2023-09-04 10:14:38 +0100

    final changes

--- src/components/CharactersList.js
+++ src/components/CharactersList.js
@@ -37,8 +37,9 @@ export default function CharactersList() {
             <div className="character-list character" key={character._id}>
               <h2>{characterName}</h2>
               <Link to={`/characters/${character._id}`}>
-                <MissingImage key={`missingImage-${character._id}`} imageUrl={character.imageUrl} altText={character.name}
-                  placeHolderImageUrl={'https://static.wikia.nocookie.net/disney/images/7/7c/Noimage.png'} />
+                {/* <MissingImage key={`missingImage-${character._id}`} imageUrl={character.imageUrl} altText={character.name}
+                  placeHolderImageUrl={'https://static.wikia.nocookie.net/disney/images/7/7c/Noimage.png'} /> */}
+                <img alt={character.name} src={character.imageUrl || 'https://static.wikia.nocookie.net/disney/images/7/7c/Noimage.png'} />
               </Link>
             </div>
           )
